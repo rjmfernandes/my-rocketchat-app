be717f4c2701a66868bcaff40957fb3ac9d3edc0
rjmfernandes <rjmfernandes@gmail.com>
2023-05-23 14:02:26 +0200

    add premessagesentprevent

--- MyRocketChatAppApp.ts
+++ MyRocketChatAppApp.ts
@@ -1,13 +1,24 @@
 import {
     IAppAccessors,
+    IHttp,
     ILogger,
+    IPersistence,
+    IRead,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IMessage } from '@rocket.chat/apps-engine/definition/messages';
+import { IPreMessageSentPrevent } from '@rocket.chat/apps-engine/definition/messages/IPreMessageSentPrevent';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 
-export class MyRocketChatAppApp extends App {
+export class MyRocketChatAppApp extends App implements IPreMessageSentPrevent {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
         logger.debug('Hello, World!');
+    }
+    async checkPreMessageSentPrevent?(message: IMessage, read: IRead, http: IHttp): Promise<boolean> {
+        return message.room.slugifiedName != 'general';
+    }
+    async executePreMessageSentPrevent(message: IMessage, read: IRead, http: IHttp, persistence: IPersistence): Promise<boolean> {
+        return message.text == 'test';
     }
 }
